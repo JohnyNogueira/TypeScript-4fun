6adb94108e0222084e7921045a062cf213342f6d
unknown <johnynogueira1@gmail.com>
2015-12-10 23:04:55 -0200

    interfaces

---
+++ src/interfaces/interfaces.js
@@ -0,0 +1,15 @@
+var Dog = (function () {
+    function Dog(name, age) {
+        this.name = name;
+        this.age = age;
+        this.hisName = name;
+    }
+    Dog.prototype.getName = function () {
+        return "His name is : " + this.hisName;
+    };
+    return Dog;
+})();
+var dog = new Dog("John Cena", 2);
+console.log(dog.getName());
+//HIS NAME IS JOHN CENA 
+//# sourceMappingURL=interfaces.js.map
---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,27 @@
+  
+interface IDog{
+	name: string;
+    breed?: string;
+    age: number; 
+		
+}
+
+class Dog implements IDog {
+	
+	hisName: string;
+	
+    constructor (public name: string, public age: number) {
+		this.hisName = name;
+    }
+	
+	getName() {
+		return "His name is : " + this.hisName;
+	}
+} 
+
+
+var dog = new Dog("John Cena" , 2);
+ 
+ 
+console.log(dog.getName());
+//HIS NAME IS JOHN CENA
